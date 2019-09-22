6a539da64d9436ef91a12d77e5c365fa8fa375e9
Marty Jiffar <marty.jiffar@gmail.com>
2019-09-22 00:42:16 -0400

    good first draft

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
     
+    $.when($("#name").fadeIn(1000))
+    .done(function() {
+        $("#resume").fadeIn(500);
+        $("#projects").fadeIn(1500);
+        $("#about").fadeIn(3000);
+        $("#contact").fadeIn(4500);
+    });
+    
     $("#projects").click(function() {
         $.when($(".homepage").fadeOut(700))
         .done(function() {
